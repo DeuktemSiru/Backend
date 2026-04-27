c1840b5cb43e744d1b4110f18118de88b9bf69fc
Kenneth Jeong <kenneth.jeong@proton.me>
2026-04-27 16:35:22 +0900

    feat: initial commit

---
+++ src/main/kotlin/com/deuktemsiru/DeuktemsiruApplication.kt
@@ -0,0 +1,11 @@
+package com.deuktemsiru
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeuktemsiruApplication
+
+fun main(args: Array<String>) {
+	runApplication<DeuktemsiruApplication>(*args)
+}
---
+++ src/test/kotlin/com/deuktemsiru/DeuktemsiruApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.deuktemsiru
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeuktemsiruApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
